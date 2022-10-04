05416fe77e100843c0735005566d8f76ca291093
Md Ali <mali54@hawk.iit.edu>
2022-10-04 14:42:24 -0500

    Final Lab

--- labs/LAB-4/lab4.7.asm
+++ labs/LAB-4/lab4.7.asm
@@ -1,139 +1,86 @@
-A:
-        addiu   $sp,$sp,-40
-        sw      $31,36($sp)
-        sw      $fp,32($sp)
-        sw      $16,28($sp)
-        move    $fp,$sp
-        sw      $4,40($fp)
-        sw      $5,44($fp)
-        lw      $2,40($fp)
-        nop
-        bne     $2,$0,$L2
-        nop
+                .data
+#        .align  2
+int_1:          .asciiz	"\nEnter a non-negative integer: "
 
-        lw      $2,44($fp)
-        nop
-        addiu   $2,$2,1
-        sw      $2,44($fp)
-        lw      $2,44($fp)
-        b       $L3
-        nop
+int_2:          .asciiz	"\nEnter another non-negative integer: "
 
-$L2:
-        lw      $2,44($fp)
-        nop
-        bne     $2,$0,$L4
-        nop
+answer:         .asciiz	"\nAckermann's function final answer: "
 
-        lw      $2,40($fp)
-        nop
-        addiu   $2,$2,-1
-        li      $5,1                        # 0x1
-        move    $4,$2
-        jal     A
-        nop
-
-        b       $L3
-        nop
-
-$L4:
-        lw      $2,40($fp)
-        nop
-        addiu   $16,$2,-1
-        lw      $2,44($fp)
-        nop
-        addiu   $2,$2,-1
-        move    $5,$2
-        lw      $4,40($fp)
-        jal     A
-        nop
-
-        move    $5,$2
-        move    $4,$16
-        jal     A
-        nop
-
-$L3:
-        move    $sp,$fp
-        lw      $31,36($sp)
-        lw      $fp,32($sp)
-        lw      $16,28($sp)
-        addiu   $sp,$sp,40
-        j       $31
-        nop
-
-$LC0:
-        .ascii  "Welcome! This program will give you answers to Ackermann"
-        .ascii  "'s function, enjoy! \000"
-$LC1:
-        .ascii  "Enter a non-negative integer: \000"
-$LC2:
-        .ascii  "%d\000"
-$LC3:
-        .ascii  "Enter another non-negative integer: \000"
-$LC4:
-        .ascii  "Ackermann's function final answer: %d\000"
+                .text
+       	        .globl main
+       	
 main:
-        addiu   $sp,$sp,-48
-        sw      $31,44($sp)
-        sw      $fp,40($sp)
-        move    $fp,$sp
-        lui     $2,%hi($LC0)
-        addiu   $4,$2,%lo($LC0)
-        jal     puts
-        nop
+	            move $s0, $0
+                la $a0, int_1
+                li $v0, 4
+                syscall
 
-        lui     $2,%hi($LC1)
-        addiu   $4,$2,%lo($LC1)
-        jal     printf
-        nop
+                li $v0, 5
+                syscall
 
-        addiu   $2,$fp,28
-        move    $5,$2
-        lui     $2,%hi($LC2)
-        addiu   $4,$2,%lo($LC2)
-        jal     __isoc99_scanf
-        nop
+                move $s1, $v0
+                la $a0, int_2
+                li $v0, 4
+                syscall
+ 	
+                li $v0, 5
+                syscall 
+                move $s2, $v0
+                move $a0, $s1
+                move $a1, $s2
+	            jal ackermann
+                move $s0, $v0 
+  
+                la $a0, answer
+                li $v0, 4
+                syscall
+            
+                li $v0, 1
+                move $a0, $s0
+                syscall 
 
-        li      $4,10                 # 0xa
-        jal     putchar
-        nop
+                li $v0, 10
+                syscall
 
-        lui     $2,%hi($LC3)
-        addiu   $4,$2,%lo($LC3)
-        jal     printf
-        nop
+ackermann:
+                addi $sp, $sp, -24
+                sw $ra, 20($sp)
+                sw $s0, 16($sp) 
+                sw $s1, 12($sp)
+                sw $s2, 8($sp)
+                sw $s3, 4($sp)
+                sw $s4, 0($sp)
 
-        addiu   $2,$fp,32
-        move    $5,$2
-        lui     $2,%hi($LC2)
-        addiu   $4,$2,%lo($LC2)
-        jal     __isoc99_scanf
-        nop
+                move $s0, $a0
+                move $s1, $a1
+                move $s2, $0
+                
+                bne $s0, 0, check_1
+                addi $s2, $s1, 1
 
-        li      $4,10                 # 0xa
-        jal     putchar
-        nop
+check_1:
+                ble $s0, 0, check_2
+                bne $s1, 0, check_2
+                addi $a0, $s0, -1
+                addi $a1, $0, 1
+                jal ackermann
+                move $s2, $v0               
 
-        lw      $2,28($fp)
-        lw      $3,32($fp)
-        nop
-        move    $5,$3
-        move    $4,$2
-        jal     A
-        nop
+check_2:        
+                ble $s0, 0, end
+                ble $s1, 0, end
+                move $a0, $s0
+                addi $a1, $s1, -1
+                jal ackermann
+                move $s2, $v0
 
-        sw      $2,24($fp)
-        lw      $5,24($fp)
-        lui     $2,%hi($LC4)
-        addiu   $4,$2,%lo($LC4)
-        jal     printf
-        nop
-
-        move    $2,$0
-        move    $sp,$fp
-        lw      $31,44($sp)
-        lw      $fp,40($sp)
-        addiu   $sp,$sp,48
-        j       $31
-        nop
+end:            
+                move $v0, $s2                 
+                lw $ra, 20($sp)
+                lw $s0, 16($sp)
+                lw $s1, 12($sp)
+                lw $s2, 8($sp)
+                lw $s3, 4($sp)               
+                lw $s4, 0($sp)
+                addi $sp, $sp, 24
+                jr $ra
---
+++ labs/LAB-4/other-code/lab4.7-v2.asm
@@ -0,0 +1,139 @@
+A:
+        addiu   $sp,$sp,-40
+        sw      $31,36($sp)
+        sw      $fp,32($sp)
+        sw      $16,28($sp)
+        move    $fp,$sp
+        sw      $4,40($fp)
+        sw      $5,44($fp)
+        lw      $2,40($fp)
+        nop
+        bne     $2,$0,$L2
+        nop
+
+        lw      $2,44($fp)
+        nop
+        addiu   $2,$2,1
+        sw      $2,44($fp)
+        lw      $2,44($fp)
+        b       $L3
+        nop
+
+$L2:
+        lw      $2,44($fp)
+        nop
+        bne     $2,$0,$L4
+        nop
+
+        lw      $2,40($fp)
+        nop
+        addiu   $2,$2,-1
+        li      $5,1                        # 0x1
+        move    $4,$2
+        jal     A
+        nop
+
+        b       $L3
+        nop
+
+$L4:
+        lw      $2,40($fp)
+        nop
+        addiu   $16,$2,-1
+        lw      $2,44($fp)
+        nop
+        addiu   $2,$2,-1
+        move    $5,$2
+        lw      $4,40($fp)
+        jal     A
+        nop
+
+        move    $5,$2
+        move    $4,$16
+        jal     A
+        nop
+
+$L3:
+        move    $sp,$fp
+        lw      $31,36($sp)
+        lw      $fp,32($sp)
+        lw      $16,28($sp)
+        addiu   $sp,$sp,40
+        j       $31
+        nop
+
+$LC0:
+        .ascii  "Welcome! This program will give you answers to Ackermann"
+        .ascii  "'s function, enjoy! \000"
+$LC1:
+        .ascii  "Enter a non-negative integer: \000"
+$LC2:
+        .ascii  "%d\000"
+$LC3:
+        .ascii  "Enter another non-negative integer: \000"
+$LC4:
+        .ascii  "Ackermann's function final answer: %d\000"
+main:
+        addiu   $sp,$sp,-48
+        sw      $31,44($sp)
+        sw      $fp,40($sp)
+        move    $fp,$sp
+        lui     $2,%hi($LC0)
+        addiu   $4,$2,%lo($LC0)
+        jal     puts
+        nop
+
+        lui     $2,%hi($LC1)
+        addiu   $4,$2,%lo($LC1)
+        jal     printf
+        nop
+
+        addiu   $2,$fp,28
+        move    $5,$2
+        lui     $2,%hi($LC2)
+        addiu   $4,$2,%lo($LC2)
+        jal     __isoc99_scanf
+        nop
+
+        li      $4,10                 # 0xa
+        jal     putchar
+        nop
+
+        lui     $2,%hi($LC3)
+        addiu   $4,$2,%lo($LC3)
+        jal     printf
+        nop
+
+        addiu   $2,$fp,32
+        move    $5,$2
+        lui     $2,%hi($LC2)
+        addiu   $4,$2,%lo($LC2)
+        jal     __isoc99_scanf
+        nop
+
+        li      $4,10                 # 0xa
+        jal     putchar
+        nop
+
+        lw      $2,28($fp)
+        lw      $3,32($fp)
+        nop
+        move    $5,$3
+        move    $4,$2
+        jal     A
+        nop
+
+        sw      $2,24($fp)
+        lw      $5,24($fp)
+        lui     $2,%hi($LC4)
+        addiu   $4,$2,%lo($LC4)
+        jal     printf
+        nop
+
+        move    $2,$0
+        move    $sp,$fp
+        lw      $31,44($sp)
+        lw      $fp,40($sp)
+        addiu   $sp,$sp,48
+        j       $31
+        nop
